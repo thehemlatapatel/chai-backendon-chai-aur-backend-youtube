937d491b58fe7b9717751059f3985b94467df65a
hema <hema@gmail.com>
2025-05-05 00:06:56 +0530

    util code discussion

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log("hello")
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app  = express()
+
+app.use(cors({
+    origin:process.evn.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limt:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ import connectDB from "./db/index.js";
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running at port:${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection faild !!!",err);
+})
 
 
 
